4b4a6553b424ac41313652caeab41c7ed88c8c81
ZyonixGaming <zyonixgaming@gmail.com>
2016-05-15 21:48:28 +0800

    Add files via upload

---
+++ links.js
@@ -0,0 +1,8 @@
+function loadLinks(){
+$('#linklist ul').append("<li data-role='list-divider'>My Pages</li>");
+$('#linklist ul').append("<li><a href='http://zyonixgaming.blogspot.com/p/ftd.html'>Blueprint Modification Tool</a></li>");
+$('#linklist ul').append("<li><a href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for this tool</a></li>");
+$('#linklist ul').append("<li data-role='list-divider'>Other Links</li>");
+$('#linklist ul').append("<li><a href='http://fromthedepthsgame.com/forum/'>From the Depths Official Forums</a></li>");
+$('#linklist ul').append("<li><a href='http://store.steampowered.com/app/268650'>From the Depths on Steam</a></li>");
+}
