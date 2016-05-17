285111f245eee6e5cc6659038cb0e67f59d449b9
ZyonixGaming <zyonixgaming@gmail.com>
2016-05-17 23:59:12 +0800

    Add files via upload

--- links.js
+++ links.js
@@ -1,8 +1,9 @@
 var LinkList=[
 "<li data-role='list-divider'>My Pages</li>",
-"<li><a data-ajax='false' href='http://zyonixgaming.blogspot.com/p/ftd.html'>Blueprint Modification Tool</a></li>",
+"<li><a data-ajax='false' href='blueprint-modification.html'>Blueprint Modification Tool</a></li>",
 "<li><a data-ajax='false' href='blueprint-compare.html'>Blueprint Compare Tool</a></li>",
 "<li><a data-ajax='false' href='blueprint-viewer.html'>Blueprint Viewer with 3D Preview</a></li>",
+"<li><a data-ajax='false' href='http://zyonixgaming.blogspot.com/p/ftd.html'>Modification Tool with basic interface</a></li>",
 "<li><a data-ajax='false' href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for this tool</a></li>",
 "<li data-role='list-divider'>Other Links</li>",
 "<li><a data-ajax='false' href='http://fromthedepthsgame.com/forum/'>From the Depths Official Forums</a></li>",
