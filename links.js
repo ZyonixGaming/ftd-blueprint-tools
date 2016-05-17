f0552686ea88c707cbc1882d70e7715b88ffd851
ZyonixGaming <zyonixgaming@gmail.com>
2016-05-17 10:22:01 +0800

    Add files via upload

--- links.js
+++ links.js
@@ -1,10 +1,10 @@
 var LinkList=[
 "<li data-role='list-divider'>My Pages</li>",
-"<li><a href='http://zyonixgaming.blogspot.com/p/ftd.html'>Blueprint Modification Tool</a></li>",
-"<li><a href='blueprint-compare.html'>Blueprint Compare Tool</a></li>",
-"<li><a href='blueprint-viewer.html'>Blueprint Viewer with 3D Preview</a></li>",
-"<li><a href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for this tool</a></li>",
+"<li><a data-ajax='false' href='http://zyonixgaming.blogspot.com/p/ftd.html'>Blueprint Modification Tool</a></li>",
+"<li><a data-ajax='false' href='blueprint-compare.html'>Blueprint Compare Tool</a></li>",
+"<li><a data-ajax='false' href='blueprint-viewer.html'>Blueprint Viewer with 3D Preview</a></li>",
+"<li><a data-ajax='false' href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for this tool</a></li>",
 "<li data-role='list-divider'>Other Links</li>",
-"<li><a href='http://fromthedepthsgame.com/forum/'>From the Depths Official Forums</a></li>",
-"<li><a href='http://store.steampowered.com/app/268650'>From the Depths on Steam</a></li>"
+"<li><a data-ajax='false' href='http://fromthedepthsgame.com/forum/'>From the Depths Official Forums</a></li>",
+"<li><a data-ajax='false' href='http://store.steampowered.com/app/268650'>From the Depths on Steam</a></li>"
 ]
