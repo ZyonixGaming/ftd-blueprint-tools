ffd3c68ef287588ec2ea39bb2366d42634a1ea00
ZyonixGaming <zyonixgaming@gmail.com>
2016-05-22 07:52:21 +0800

    Add files via upload

---
+++ js/settingsmap.js
@@ -0,0 +1,116 @@
+settingsTable = {
+	"aea2161d-ff43-4b10-ac0e-933fb5809faa":{ //aerial AI
+		0:{
+			"name":"Angle deviation before turn started"
+		},
+		1:{
+			"name":"Angle deviation before we roll to turn"
+		},
+		2:{
+			"name":"The most extreme roll we aim for to facilitate a roll-based turn"
+		},
+		3:{
+			"name":"Cruising altitude"
+		},
+		4:{
+			"name":"Minimum altitude"
+			
+		},
+		5:{
+			"name":"Distance beyond which we begin attack run"
+		},
+		6:{
+			"name":"Distance below which we abort attack run"
+		},	
+		7:{
+			"name":"Elapsed time before we force another attack run"
+		},	
+	},
+	"f7042d78-d7ae-4a0b-b273-197813c61648":{ //shield Projector
+		0:{
+			"name":"Range"
+		},
+		1:{
+			"name":"Height"
+		},
+		2:{
+			"name":"Width"			
+		},
+		3:{
+			"name":"Effect Strength"
+		},
+		4:{
+			"name":"Mode",
+			"values":["None!","Disrupt!","Reflect!","Laser Absorb!"]
+		},
+		5:{
+			"name":"Azimuth"
+		},
+		6:{
+			"name":"Elevation"
+		}		
+		
+	},	
+	"6ae7c977-3f62-414e-98aa-5dc0da89b376":{ //naval AI
+		0:{
+			"name":"Enter broadside below this range"
+		},
+		1:{
+			"name":"Leave broadside above this range"		
+		},
+		2:{
+			"name":"Nominal broadside angle"		
+		},
+		3:{
+			"name":"Broadside minimum range"	
+		},
+		4:{
+			"name":"Idle approach distance"	
+		},
+		5:{
+			"name":"Turning Circle"	
+		},
+		6:{
+			"name":"Depth Requirement"	
+		},
+		7:{
+			"name":"Reverse",
+			"values":["enabled","disabled"]
+		}
+	},
+	"2c557c38-4136-436a-bae2-c5cd1102690c":{ //target prioritization
+		0:{
+			"name":"Set value per block"
+		},
+		1:{
+			"name":"Set value per weapon"		
+		},
+		2:{
+			"name":"Set value per crew/player/???"		
+		},
+		3:{
+			"name":"Set value per propulsion"	
+		},
+		4:{
+			"name":"Set value per engine"	
+		},
+		5:{
+			"name":"Set value per range"	
+		},
+		6:{
+			"name":"Set value per speed"	
+		},
+		7:{
+			"name":"Set value per altitude"
+		}
+	},
+	"31277649-04ec-45db-adfc-4a7d0a73686a":false, //ammo barrel
+	"82b2446e-c12e-436a-8340-76d38372ead1":false //ammo parts box
+	
+
+}
+
+
+
+
+
