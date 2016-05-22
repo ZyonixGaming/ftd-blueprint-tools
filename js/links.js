d212c52e9b2166d4b5d69db16fc50f12a52b1d80
ZyonixGaming <zyonixgaming@gmail.com>
2016-05-22 11:53:51 +0800

    Add files via upload

--- js/links.js
+++ js/links.js
@@ -7,7 +7,7 @@ var LinkList=[
 "<li><a data-ajax='false' href='blueprint-viewer.html'>Blueprint Viewer</a></li>",
 "<li><a data-ajax='false' href='schematic-blueprint-converter.html'>Schematic to Blueprint Converter</a></li>",
 "<li><a data-ajax='false' href='http://zyonixgaming.blogspot.com/p/ftd.html'>Modification Tool with basic interface</a></li>",
-"<li><a data-ajax='false' href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for this tool</a></li>",
+"<li><a data-ajax='false' href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for FTD Blueprint Tools</a></li>",
 "<li data-role='list-divider'>Other Links</li>",
 "<li><a data-ajax='false' href='http://fromthedepthsgame.com/forum/'>From the Depths Official Forums</a></li>",
 "<li><a data-ajax='false' href='http://store.steampowered.com/app/268650'>From the Depths on Steam</a></li>"
