7b65b7e28c47131fe2fe001b0486e8a0fe87515f
ZyonixGaming <zyonixgaming@gmail.com>
2016-09-08 15:20:07 +0800

    Add files via upload

--- js/preview3d.js
+++ js/preview3d.js
@@ -1,3 +1,25 @@
+THREE.CustomCubeGeometry = function( vertices ,colors) {
+	THREE.Geometry.call( this );
+	var faces = [[0,1,2],[3,2,1],[3,7,2],[6,2,7],[2,6,0],[4,0,6],[1,0,5],[4,5,0],[4,6,5],[7,5,6],[3,1,7],[5,7,1] ]; 
+	
+	for(var f=0;f<8;f++){
+		this.vertices.push(vertices[f]);
+		
+	}
+
+	for ( var i = 0; i < faces.length; i ++ ) {
+		this.faces.push( new THREE.Face3(
+				faces[ i ][ 0 ],
+				faces[ i ][ 1 ],
+				faces[ i ][ 2 ], null,colors
+		) );
+	}
+};
+THREE.CustomCubeGeometry.prototype = Object.create( THREE.Geometry.prototype );
+THREE.CustomCubeGeometry.prototype.constructor = THREE.CustomCubeGeometry;
+
+
+
 var container;
 var camera, controls, scene, renderer;
 var animateid=null;
@@ -13,6 +35,7 @@ var pointlight=null;
 var arrowHelper=null;
 
 function init() {
+	var temptime=(new Date()).getTime();
 	
 	if(!alreadyinit){
 		alreadyinit=true;
@@ -27,37 +50,17 @@ function init() {
 		alreadyanimate=true;
 		animate();
 	}
+	
+	var temptime2=(new Date()).getTime();
+	var timetaken = (temptime2-temptime);
+	//console.log("time taken: " + timetaken);	
 }
 
 function firstInit(){
-	
 	container = document.getElementById( "container" );
-
 	scene = new THREE.Scene();
 	scene.add( new THREE.AmbientLight( 0x555555 ) );
 
-
-	
-	
-	var minx=0;
-	var maxx=0;
-	var miny=0;
-	var maxy=0;
-	var minz=0;
-	var maxz=0;
-	
-	var midx=0;
-	var midy=0;
-	var midz=0;
-	
-	var maxdist=Math.sqrt((Math.pow(Math.max(maxx-minx,maxy-miny,maxz-minz)/1.4,2))/3);
-	maxdist=maxdist/2;
-	maxdist=maxdist+10/maxdist;
-	maxdist=Math.sqrt(Math.pow(maxdist,2)/3);
-	
-	
-
-	
 	
 	var sgeo = new THREE.SphereGeometry(1);
 	var smat = new THREE.MeshBasicMaterial( {color: 0xffff00,transparent: true} );
@@ -71,15 +74,14 @@ function firstInit(){
 	scene.add( sphere );
 	
 	
-	
-	var intensity=Math.sqrt(maxz-minz)/6
+	var intensity=1;
 	pointlight = new THREE.PointLight( 0xffffff, intensity );
-	pointlight.position.set( (maxx*(-sizescale))+250, (maxy+Math.sqrt(maxy-miny))*(sizescale+1), maxz*(sizescale+1) );
+	pointlight.position.set( 0,0,0 );
 	scene.add( pointlight );
 
 	var dir = new THREE.Vector3( 0, 0, 1 );
-	var origin = new THREE.Vector3( (minx-2)*(-sizescale), (miny-1)*sizescale, (midz*0.8)*sizescale) ;
-	var al = (maxz-minz)*0.2;
+	var origin = new THREE.Vector3(0,0,0) ;
+	var al = 1;
 	var ac = 0x007f00;
 
 	arrowHelper = new THREE.ArrowHelper( dir, origin, al, ac );
@@ -93,41 +95,46 @@ function firstInit(){
 	renderer.sortObjects = false;
 	container.innerHTML = "";
 	container.appendChild( renderer.domElement );
+	
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 5000 );
-	camera.position.x = (minx-maxdist)*(-sizescale);
-	camera.position.y = (maxy*1.2)*(sizescale);
-	camera.position.z = (((midz+(maxz*2.5))/3.5)*sizescale)
-	//camera.lookAt(new THREE.Vector3( midx*-10, (maxy-(miny*1.3))*10, ((maxz*1.1)-minz)*10 ));
+	
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 0;
+
 	controls = new THREE.OrbitControls( camera ,renderer.domElement);
 	controls.rotateSpeed = 1.0;
 	controls.zoomSpeed = 1.2;
 	controls.panSpeed = 0.8;
 	controls.enableZoom = true;
 	controls.enablePan = true;
-	controls.target.set(midx*(-sizescale), (((maxy+(miny*1.3))/2)*sizescale), (((minz+(maxz*2.5))/3.5)*sizescale));
-	camera.lookAt(new THREE.Vector3(midx*(-sizescale), (((maxy+(miny*1.5))/2.5)*sizescale), (((minz+(maxz*2.5))/3.5)*sizescale)));
+	controls.target.set(0,0,0);
+	camera.lookAt(new THREE.Vector3(0,0,0));
 	
 	// EVENTS
 	window.addEventListener( 'resize', onWindowResize, false );
 	onWindowResize();
 	//onWindowResize();	
-	
-	
 }
 
 
 function renderObject(){
-	
-
-	var geometry = new THREE.Geometry();
-	var defaultMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, shininess: 0	} );
-
-
 
 	
 	var color = new THREE.Color();
+	var geometry = new THREE.Geometry();
+
+
+	var defaultMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors, shininess: 5	} );
+	
+	
 	var matrix = new THREE.Matrix4();
 	var quaternion = new THREE.Quaternion();	
+	
+	var scale = new THREE.Vector3();
+	scale.x = sizescale;
+	scale.y = sizescale;
+	scale.z = sizescale;	
 
 	for(renderIndex in blockRenderData){
 		var tempRender = blockRenderData[renderIndex];
@@ -138,15 +145,11 @@ function renderObject(){
 		position.z = (tempRender.position[2])*sizescale;
 
 
-		quaternion = new THREE.Quaternion(-tempRender.rotation[0],tempRender.rotation[1],tempRender.rotation[2],-tempRender.rotation[3]);			
-		//quaternion = new THREE.Quaternion(0,0,0,1);			
-		var scale = new THREE.Vector3();
-		scale.x = sizescale;
-		scale.y = sizescale;
-		scale.z = sizescale;
+		quaternion = new THREE.Quaternion(-tempRender.rotation[0],tempRender.rotation[1],tempRender.rotation[2],-tempRender.rotation[3]);					
+
 		
 		matrix.compose( position, quaternion, scale );
-		
+
 		var points = [
 			new THREE.Vector3( -(tempRender.negx-0.5), tempRender.negy-0.5, tempRender.negz-0.5 ),
 			new THREE.Vector3( -(tempRender.posx+0.5), tempRender.negy-0.5, tempRender.negz-0.5 ),
@@ -157,16 +160,11 @@ function renderObject(){
 			new THREE.Vector3( -(tempRender.negx-0.5), tempRender.posy+0.5, tempRender.posz+0.5 ),
 			new THREE.Vector3( -(tempRender.posx+0.5), tempRender.posy+0.5, tempRender.posz+0.5 )
 		];
-		var geom =  new THREE.ConvexGeometry( points );		
+		var geom =  new THREE.CustomCubeGeometry( points ,color.set(tempRender.color));		
 
-	
-		//var geom = new THREE.BoxGeometry( 1, 1, 1 );
-		//applyVertexColors( geom, color.setHex( Math.random() * 0xffffff ) );
-		applyVertexColors(geom, color.set(tempRender.color));
 		geometry.merge( geom, matrix );	
 		
 	}
-	
 	
 	var minx=minc[0];
 	var maxx=maxc[0];
@@ -197,6 +195,8 @@ function renderObject(){
 		drawnObject=null;
 	}
 	*/
+	//geometry.mergeVertices();
+	
 	drawnObject = new THREE.Mesh( geometry, defaultMaterial );
 	scene.add( drawnObject );
 	
@@ -223,7 +223,6 @@ function renderObject(){
 	controls.target.set(midx*(-sizescale), (((maxy+(miny*1.3))/2)*sizescale), (((minz+(maxz*2.5))/3.5)*sizescale));
 	camera.lookAt(new THREE.Vector3(midx*(-sizescale), (((maxy+(miny*1.5))/2.5)*sizescale), (((minz+(maxz*2.5))/3.5)*sizescale)));
 
-	
 }
 
 function animate() {
@@ -288,15 +287,6 @@ function onWindowResize() {
 
 	render();
 
-}
-
-function applyVertexColors( g, c ) {
-	g.faces.forEach( function( f ) {
-		var n = ( f instanceof THREE.Face3 ) ? 3 : 4;
-		for( var j = 0; j < n; j ++ ) {
-			f.vertexColors[ j ] = c;
-		}
-	} );
 }
 
 function disposeNode (node)
