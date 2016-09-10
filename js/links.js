373dc5d1bd083141293a0bba38ac2d1136f5457b
ZyonixGaming <zyonixgaming@gmail.com>
2016-09-10 16:10:38 +0800

    Add files via upload

--- js/links.js
+++ js/links.js
@@ -1,10 +1,8 @@
 var LinkList=[
 "<li data-role='list-divider'>My Pages</li>",
 "<li><a data-ajax='false' href='blueprint-modification.html'>Blueprint Modification Tool</a></li>",
-"<li><a data-ajax='false' href='blueprint-block-finder.html'>Blueprint Block Finder</a></li>",
 "<li><a data-ajax='false' href='blueprint-combiner.html'>Blueprint Compare and Combine Tool</a></li>",
-"<li><a data-ajax='false' href='schematic-blueprint-converter.html'>Schematic to Blueprint Converter</a></li>",
-"<li><a data-ajax='false' href='mass-blueprint-modification.html'>Bulk Modification Tool</a></li>",
+"<li><a data-ajax='false' href='blueprint-block-finder.html'>Blueprint Block Finder</a></li>",
 "<li><a data-ajax='false' href='blueprint-modification-1.9572.html'>Old Modification Tool (FTD v1.9572)</a></li>",
 "<li><a data-ajax='false' href='http://www.fromthedepthsgame.com/forum/showthread.php?tid=19547'>Forum thread for FTD Blueprint Tools</a></li>",
 "<li data-role='list-divider'>Other Links</li>",
